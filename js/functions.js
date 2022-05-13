76829fe3f95f003c7af8264662f0bec48b1225f4
Sreejith KS <sreejithksgupta2255@gmail.com>
2022-05-13 14:45:11 +0530

    sreejith scoreset half

--- js/functions.js
+++ js/functions.js
@@ -9,3 +9,21 @@ function setscores() {
         document.getElementById('goals').innerHTML = 'goals: ' + scores.goals;
     });
 }
+
+function updatescore(sc, lv, g) {
+
+    var ref = firebase.database().ref('users/' + user.uid + '/scores/');
+    ref.once('value', function(snapshot) {
+        var scores = snapshot.val();
+        scores.score += sc;
+        scores.level += lv;
+        scores.goals += g;
+        ref.set({
+            score: score,
+            level: level,
+            goals: goals,
+        });
+
+    });
+
+}
