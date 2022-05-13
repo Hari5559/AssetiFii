f4325f54c7e94038b35af69200f1236ffad0c095
Sreejith KS <sreejithksgupta2255@gmail.com>
2022-05-13 22:01:08 +0530

    sreejith after  anurag went

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,9 +1,30 @@
-function changeapplet(page) {
+carouselcall();
 
-    console.log(page);
+
+function changeapplet(page) {
     var applets = document.getElementsByClassName("appbody");
     for (var i = 0; i < applets.length; i++) {
         applets[i].style.display = "none";
     }
     document.getElementById(page).style.display = "block";
 }
+
+var j = 0;
+
+function carouselcall() {
+    setInterval(function() {
+        car();
+        j = j % 3 + 1;
+    }, 1000);
+
+}
+
+function car() {
+    var carimgs = document.getElementsByClassName("caa");
+    for (i = 0; i < carimgs.length; i++) {
+        carimgs[i].style.display = "none";
+    }
+    carimgs[j].style.display = "block";
+}
+
+carouselcall();
