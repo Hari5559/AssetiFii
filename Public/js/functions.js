1b85e2d549ee8ca7368bd733dc6dbf045b8ce234
Ben-A-Sabu <benasabu123@gmail.com>
2022-05-14 18:07:46 +0530

    HVJVHG

--- Public/js/functions.js
+++ Public/js/functions.js
@@ -37,7 +37,7 @@ function setscores() {
 ////////////////////////// update the score of user //////////////////////////////
 function updatescore(sc) {
 
-    var ref = firebase.database().ref('users/' + firebase.auth().currentUser.uid + '/scores/');
+    var ref = firebase.database().ref('users/' + firebase.auth().currentUser.u + '/scores/');
     ref.once('value', function(snapshot) {
         var scores = snapshot.val();
         var oldscore = scores.score % 100;
