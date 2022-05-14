5a455b16a3518001a0473282304a7dcd04321738
Sreejith KS <sreejithksgupta2255@gmail.com>
2022-05-14 08:30:13 +0530

    rthsrth

--- Public/js/functions.js
+++ Public/js/functions.js
@@ -34,3 +34,33 @@ function updatescore(sc, lv, g) {
 }
 
 ////////////////////////////// add the expence of user //////////////////////////////
+
+
+
+
+
+
+
+///////////////////// fetch Stock Data /////////////////////////////////
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+        'X-RapidAPI-Key': '4765478682mshd45354dbd80fffep1c18c4jsnf34147d76223'
+    }
+};
+
+fetch('https://yh-finance.p.rapidapi.com/auto-complete?q=india&region=IN', options)
+    .then(response => response.json())
+    .then(response => showstockdata(response))
+    .catch(err => console.error(err));
+
+/////////////////////////////////// set stock  /////////////////////////////////////
+function showstockdata(data) {
+    var quotes = data.quotes;
+    var stockdata = '';
+    for (var i = 0; i < quotes.length; i++) {
+        stockdata += '<li class="list-group-item"><div class="stcknme">' + quotes[i].shortname + ' </div> <div class="stckscre">Score:' + quotes[i].score + '</div></li>';
+    }
+    id('stocklist').innerHTML = stockdata;
+}
